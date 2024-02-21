b323601d0d762f623e7251845d56bc3d7eefe237
avichayJit <94782810+avichayJit@users.noreply.github.com>
2024-02-21 11:03:38 +0200

    Update retry-curl.sh

--- retry-curl.sh
+++ retry-curl.sh
@@ -11,7 +11,7 @@ retry_curl() {
         if [ $curl_exit_code = 0 ]; then
             status_code=${response: -3}  # Extract the status code from the response
             if [[ $status_code != 104 && $status_code != 107 && $status_code != 502 && $status_code != 503 && $status_code != 504 ]]; then
-                # If status code is not connection reset (104/107) or service unavailable (502/503/504) => break and don't retry 
+                # If status code is not connection reset (104/107) or service unavailable (502/503/504) => break and don't retry
                 echo "$response"
                 return
             fi
